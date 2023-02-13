e1d0d92a74bfb03bedba6a0049414d0dab98ab1f
Khanh Le <91043526+ibretsam@users.noreply.github.com>
2023-02-13 19:14:23 +0700

    Added login screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,57 +15,69 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
-      {/* <View style={styles.heroTitles}>
-        <Text>Hello</Text>
-        <Text>Again</Text>
-        <Text>Welcome back you’ve been missed</Text>
-      </View> */}
+      <View style={styles.heroContent}>
+        <View>
+          <Text style={[styles.blackText, styles.heroTitle]}>Hello</Text>
+          <Text style={[styles.blueText, styles.heroTitle]}>Again!</Text>
+        </View>
+        <Text style={styles.heroDescription}>Welcome back you’ve</Text>
+        <Text style={styles.heroDescription}>been missed</Text>
+      </View>
 
-      {/* <View>
-        <Text>Username</Text>
-        <TextInput />
+      <View>
+        <Text style={styles.inputLabel}>
+          Username<Text style={styles.redText}>*</Text>
+        </Text>
+        <TextInput style={styles.input} />
       </View>
 
       <View>
         <View>
-          <Text>Password</Text>
-          <TextInput />
+          <Text style={styles.inputLabel}>
+            Password<Text style={styles.redText}>*</Text>
+          </Text>
+          <TextInput style={styles.input} />
         </View>
-        <View>
-          <View>
-            <BouncyCheckbox />
-            <Text>Remember me</Text>
+        <View style={styles.otherInput}>
+          <View style={styles.rememberMe}>
+            <BouncyCheckbox
+              size={20}
+              fillColor={'#1877F2'}
+              innerIconStyle={{borderRadius: 3}}
+              iconStyle={{borderRadius: 3}}
+            />
+            <Text style={styles.rememberMeText}>Remember me</Text>
           </View>
           <Pressable>
-            <Text>Forget password</Text>
+            <Text style={styles.forgotPasswordText}>Forget password</Text>
           </Pressable>
         </View>
       </View>
 
-      <Pressable>
-        <Text>Login</Text>
+      <Pressable style={styles.heroBtn}>
+        <Text style={styles.heroBtnText}>Login</Text>
       </Pressable>
 
-      <Text>or continue with</Text>
+      <Text style={styles.continueText}>or continue with</Text>
 
-      <View>
-        <Pressable>
-          <Image source={fbLogo} />
+      <View style={styles.socialBtns}>
+        <Pressable style={[styles.socialBtn, {marginRight: 16}]}>
+          <Image source={fbLogo} style={{marginRight: 10}} />
           <Text>Facebook</Text>
         </Pressable>
-        <Pressable>
-          <Image source={googleLogo} />
+        <Pressable style={styles.socialBtn}>
+          <Image source={googleLogo} style={{marginRight: 10}} />
           <Text>Google</Text>
         </Pressable>
       </View>
 
-      <Text>don’t have an account ?  </Text>
-      <Pressable>
-        <Text>Sign Up</Text>
-      </Pressable> */}
+      <View style={styles.signUp}>
+        <Text>don’t have an account ? </Text>
+        <Pressable>
+          <Text style={styles.blueText}>Sign Up</Text>
+        </Pressable>
+      </View>
     </View>
-    
   );
 };
 
@@ -74,10 +86,142 @@ export default Login;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'pink'
+    marginStart: 20,
+    marginEnd: 20,
+    flexDirection: 'column',
   },
-  heroTitles: {
-    
-  }
+  heroContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    padding: 0,
+    marginBottom: 50,
+  },
+  heroTitle: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: 48,
+    lineHeight: 72,
+  },
+  heroDescription: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 20,
+    lineHeight: 30,
+    color: '#4E4B66',
+  },
+  inputLabel: {
+    fontFamily: 'Poppins',
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 21,
+    color: '#4E4B66',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  input: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    padding: 10,
+    backgroundColor: 'white',
+    borderColor: '#4E4B66',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 6,
+  },
+  otherInput: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  rememberMe: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  rememberMeText: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 21,
+    color: '#4E4B66',
+  },
+  forgotPasswordText: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 21,
+    color: '#5890FF',
+  },
+  heroBtn: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 24,
+    paddingVertical: 13,
+    backgroundColor: '#1877F2',
+    borderRadius: 6,
+  },
+  heroBtnText: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 24,
+    color: 'white',
+  },
+  continueText: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 21,
+    color: '#4E4B66',
+    paddingVertical: 15,
+    textAlign: 'center',
+  },
+  socialBtns: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: 20,
+  },
+  socialBtn: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 12,
+    paddingRight: 24,
+    paddingLeft: 16,
+    backgroundColor: '#EEF1F4',
+    borderRadius: 6,
+    width: 174,
+    height: 48,
+  },
+
+  signUp: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  blackText: {
+    color: '#050505',
+  },
+  blueText: {
+    color: '#1877F2',
+  },
+  redText: {
+    color: '#C30052',
+  },
 });
