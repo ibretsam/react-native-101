9092d962a11238ab7a152aa52416943a1e562c74
Khanh Le <masterbruculous@gmail.com>
2023-02-16 00:46:38 +0700

    Finish NewsList Screen

---
+++ src/components/NewsItem.tsx
@@ -0,0 +1,71 @@
+import {View, Text, Image, StyleSheet, Dimensions} from 'react-native';
+import React from 'react';
+import moment from 'moment';
+
+const NewsItem = (props: any) => {
+  const data = props.data;
+  const formattedTime = moment(data.createdAt).fromNow();
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{uri: data.image}} />
+      <View style={styles.text}>
+        <Text style={styles.title}>
+          {data.title.slice(0, 60)}
+          {data.title.length > 60 ? '...' : ''}
+        </Text>
+        <Text style={styles.content}>{data.content.slice(0, 67)}...</Text>
+        <View style={styles.time}>
+          <Image
+            style={styles.clock}
+            source={require('../../assets/Time.png')}
+          />
+          <Text>{formattedTime}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default NewsItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginHorizontal: 15,
+    marginVertical: 10,
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  text: {
+    justifyContent: 'space-between',
+  },
+  title: {
+    width: Dimensions.get('window').width - 16 - 120,
+    fontWeight: '700',
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#000000',
+  },
+  content: {
+    width: Dimensions.get('window').width - 16 - 120,
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 21,
+    color: '#4E4B66',
+  },
+  time: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  clock: {
+    width: 10,
+    height: 10,
+    marginRight: 5,
+  },
+});
--- src/pages/NewsList.tsx
+++ src/pages/NewsList.tsx
@@ -1,32 +1,94 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import {Image, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import NewsItem from '../components/NewsItem';
 import {BASE_URL} from '../utils/Constants';
 
 const NewsList = (props: any) => {
   const [newsList, setNewsList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const setNewsList = async () => {
-      const response = await axios.get(`${BASE_URL}/articles`);
-      console.log(response);
+    const getNewsList = async () => {
+      const token = await AsyncStorage.getItem('token');
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      };
+      const response = await axios.get(`${BASE_URL}/articles`, config);
+      setNewsList(response.data.data);
+      setIsLoading(false);
     };
 
-    setNewsList();
+    getNewsList();
   }, []);
 
   return (
-    <View>
-      <View>
-        <Image source={require('../../assets/Logo.png')} />
-        <Image source={require('../../assets/Notification.png')} />
-      </View>
-      <View>
-        <Text>Latest</Text>
-        <Text>See all</Text>
-      </View>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      {isLoading ? (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" color="#1877F2" />
+          <Text>Loading</Text>
+        </View>
+      ) : (
+        <View>
+          <View style={styles.topBar}>
+            <Image source={require('../../assets/Logo.png')} />
+            <Image source={require('../../assets/Notification.png')} />
+          </View>
+          <View style={styles.filter}>
+            <Text style={styles.latest}>Latest</Text>
+            <Text>See all</Text>
+          </View>
+          <FlatList
+            data={newsList}
+            renderItem={({item}) => <NewsItem data={item} reversed />}
+          />
+        </View>
+      )}
+    </SafeAreaView>
   );
 };
 
 export default NewsList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+    alignItems: 'center',
+  },
+  filter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+  },
+  latest: {
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
